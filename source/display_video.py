ed7e78c0e77c2d3e13cc8f6c389f2495a05116f9
kaledj <kaledj@appstate.edu>
2014-08-01 11:24:49 -0400

    Temporal difference

--- source/display_video.py
+++ source/display_video.py
@@ -16,57 +16,31 @@ def show_video(filename):
     frame_num= int(vidcapture.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))
     fps = vidcapture.get(cv2.cv.CV_CAP_PROP_FPS)
 
-    ret, frame = vidcapture.read()
-    vres, hres, channels = frame.shape
-
-    # Initialize bg model
-    frameBuffer = np.zeros((100, vres, hres))
-    for i in range(100):
-        ret, frame = vidcapture.read()
-        frame2Buff = cv2.cvtColor(frame, cv2.cv.CV_BGR2GRAY)
-        #cv2.GaussianBlur(frame2Buff, (5,5), 0, frame2Buff, 0)
-        #frameBuffer[i] = cv2.equalizeHist(frame2Buff)
-        frameBuffer[i] = frame2Buff
-    bgModel = segmentation.initBGM(frameBuffer)
-    cv2.imshow("Background Model", bgModel)
-    initialBG = bgModel.copy()
+    ret, prevFrame = vidcapture.read()
+    prevFrame = cv2.cvtColor(prevFrame, cv2.cv.CV_BGR2GRAY)
 
     while ret and cv2.waitKey(int(1/fps*1000)) != 27:
-        frameGray = cv2.cvtColor(frame, cv2.cv.CV_BGR2GRAY)
+        ret, currFrame = vidcapture.read()
+        currFrame = cv2.cvtColor(currFrame, cv2.cv.CV_BGR2GRAY)
+        dFrame = np.abs(prevFrame.astype(np.int) - currFrame.astype(np.int)).astype(np.uint8)
+        cv2.imshow("Previous frame", prevFrame)
+        prevFrame = currFrame.copy()
+        motionImage = ((dFrame >= 20) * 255).astype(np.uint8)
         
-        #frameGray = cv2.equalizeHist(frameGray)
+        cv2.imshow("Difference", dFrame)
+        cv2.imshow("Motion", motionImage)
 
-        #cv2.GaussianBlur(frameGray, (5,5), 0, frameGray, 0)
-        cv2.putText(frame, "Frame: %d FPS: %d"%(frame_num, fps), 
-            (0, vres-2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 
-            lineType=cv2.CV_AA)
-        
-        # Get mask using current background model and filter it
-        dFrame = np.abs(bgModel.astype(np.int) - frameGray.astype(np.int))
-        threshold, fgmask = segmentation.fgMask(dFrame)
-
-        kernel = np.ones((3,3), np.uint8)
-        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel, iterations=1)
-        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel, iterations=1)
-        # Show masked frame
-        #cv2.imshow("frameWindow", frame * np.dstack((fgmask, fgmask, fgmask)))
-        cv2.imshow("frameWindow", frameGray * fgmask)
-        cv2.imshow("Background Model", bgModel)
-        cv2.imshow("Difference Image", dFrame.astype(np.uint8))
-        cv2.imshow("Grayscale input", frame)
-        # Update background model
-        #currentBgModel = bgModel.copy()
-        segmentation.updateBGM_alr(bgModel, dFrame, frameGray, threshold, fgmask)
-        #print "BG Change:", currentBgModel.astype(np.int) - bgModel.astype(np.int)
-
-        ret, frame = vidcapture.read()
-        frame_num += 1
+        contours, hierarchy = cv2.findContours(motionImage, cv2.RETR_TREE, 
+            cv2.CHAIN_APPROX_TC89_L1)
+        cv2.drawContours(currFrame, contours, -1, (0, 0, 255), hierarchy=hierarchy, maxLevel=2)
+        cv2.imshow("Current frame", currFrame)
     vidcapture.release()
-    cv2.destroyWindow("frameWindow")
+    cv2.destroyAllWindows()
 
 def main():
     show_video("../videos/721test.mkv")
-    show_video("../videos/live_video/test.avi")
+    show_video("../videos/video1.mkv")
+    show_video("../videos/724video.mkv")
 
 if __name__ == '__main__':
     main()
