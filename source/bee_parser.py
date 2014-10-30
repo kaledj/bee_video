7e4b493d0c9faab71b6927fc9bf6817e57d8c5d7
Scott <scottshuffler@appsrc2.cs.appstate.edu>
2014-10-30 13:08:48 -0400

    Added bee_parser to count bees in a frame

--- source/bee_parser.py
+++ source/bee_parser.py
@@ -1,4 +1,4 @@
-__author__ = 'shuffleres'
+__authors__ = 'shuffleres', 'smithc4'
 
 import numpy as np
 import cv2
@@ -6,8 +6,18 @@ import cv
 from video_loader import load_local
 import segmentation, features
 import time
+import sys
 
-def parser(videoname, storefile):
+countClicks = 0
+
+def parser(videoname):
+
+    a,b,c = videoname.split("/")
+    stfile = c.split(".")
+    global storefile
+    storefile = stfile[0]
+    global string
+    string = ""
     vidcapture = load_local(videoname)
     nframes = int(vidcapture.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
     frame_num = int(vidcapture.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))
@@ -23,7 +33,12 @@ def parser(videoname, storefile):
 
     msPerFrame = 1 / fps * 1000
     countFrame = 0
-    countClicks = 0
+    cv2.namedWindow("Current frame")
+    cv2.setMouseCallback("Current frame", on_mouse)
+    print("Press ESC to exit with changes.")
+    print("Press Q to exit without changes.")
+    print("Press the spacebar for the next frame.")
+    print("Press the left mouse button on the frame to count the bees.")
     while(ret):
         timei = time.clock()*1000
         timef = time.clock()*1000
@@ -31,65 +46,53 @@ def parser(videoname, storefile):
         framerate = framerate*.1 + .9*max(1 / 60*1000, 1 / msPerFrame*1000)
         wait = max(int(msPerFrame-timed), 1)
         key = cv2.waitKey(wait)
-
+        if key is 113: exit()
         if key is 27: break
         if key is 32:
             countFrame = countFrame + 1
-            print("Frames: " + str(countFrame))
+
+            
+           
             prevFrame = currFrame.copy()
             ret, currFrame = vidcapture.read()
 
             if ret:
-                if cv2.EVENT_LBUTTONDOWN:
-                    drawing = True
-                    countClicks = countClicks + 1
-                    cv2.EVENT_LBUTTONUP = True
                 colorFrame = currFrame.copy()
                 currFrame = cv2.cvtColor(currFrame, cv2.cv.CV_BGR2GRAY)
                 ix,iy = -1,-1
-
-                print("Clicks: " + str(countClicks))
-                    #storefile.write("Frame: " + i + " X: " + ix + " Y: " + iy)
+                string += ("Frame: " + str(countFrame) + "\n")
+                string += ("Clicks: " + str(countClicks) + "\n\n")
+                print "Frame: " + str(countFrame) + "\n"
+                global countClicks
+                countClicks = 0
         cv2.imshow("Current frame", colorFrame)
-
+    if string:
+        string += ("Frame: " + str(countFrame+1) + "\n")
+        string += ("Clicks: " + str(countClicks) + "\n")
+        f = open("../ground_truths/" + storefile + ".txt",'w')
+        print(string)
+        f.write(string)
+        f.close()
+    
 
 def on_mouse(event, x, y, flags, params):
+    global countClicks
+    global string
+
     if event == cv.CV_EVENT_LBUTTONDOWN:
-        print 'Start Mouse Position: '+str(x)+', '+str(y)
-        sbox = [x, y]
-        boxes.append(sbox)
-    elif event == cv.CV_EVENT_LBUTTONUP:
-        print 'End Mouse Position: '+str(x)+', '+str(y)
-        ebox = [x, y]
-        boxes.append(ebox)
+        
+        string += ("Position: " + str(x) + ', ' + str(y) + "\n")
+        print(string)
+        countClicks += 1;
 
-
-                #drawing = False # true if mouse is pressed
-                #mode = True # if True, draw rectangle. Press 'm' to toggle to curve
-
-#def on_mouse(event, x, y, flag, params):
-    #global start_draw
-    #global roi_x0
-    #global roi_y0
-    #global roi_x1
-   # global roi_y1
-    #if (event == cv.CV_EVENT_LBUTTONDOWN):
-   # if (not start_draw):
-   #     roi_x0 = x
-     #   roi_y0 = y
-        #start_draw = True
-   # else:
-      #  roi_x1 = x
-      ##  roi_y1 = y
-        #start_draw = False
-    #elif (event == cv.CV_EVENT_MOUSEMOVE and start_draw):
-        #Redraw ROI selection
-       # image2 = cv.CloneImage(image)
-       # cv.Rectangle(image2, (roi_x0, roi_y0), (x, y),cv.CV_RGB(255, 0, 255), 1)
-        #cv.ShowImage(window_name, image2)
+def exit():
+    cv2.destroyAllWindows()
+    sys.exit()
 
 def main():
-    parser("../videos/easy_video.h264", "test.txt")
+    
+    parser("../videos/729long.mkv")
+    
 
 if __name__ == '__main__':
     main()
