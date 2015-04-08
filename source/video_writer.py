8acef2e5b515a9dbb5e7ad6c968659f9ba86c9a7
kaledj <kaledj@appstate.edu>
2015-04-08 16:57:50 -0400

    Delete old unused files.

--- source/video_writer.py
+++ source/video_writer.py
@@ -10,29 +10,29 @@ import cv2
 from video_loader import load_local
 
 def save_locally(inputfilename, outfilename):
-	# Set up input stream and get info
-	vidcapture = load_local(inputfilename)
-	print vidcapture.isOpened()
-	print vidcapture.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 0.0)
-	print vidcapture.get(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO)
-	print vidcapture.get(cv2.cv.CV_CAP_PROP_POS_MSEC)
-	print vidcapture.get(0xFF)
+    # Set up input stream and get info
+    vidcapture = load_local(inputfilename)
+    print vidcapture.isOpened()
+    print vidcapture.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 0.0)
+    print vidcapture.get(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO)
+    print vidcapture.get(cv2.cv.CV_CAP_PROP_POS_MSEC)
+    print vidcapture.get(0xFF)
 
-	fps = int(vidcapture.get(cv2.cv.CV_CAP_PROP_FPS))
-	width = int(vidcapture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
-	height = int(vidcapture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+    fps = int(vidcapture.get(cv2.cv.CV_CAP_PROP_FPS))
+    width = int(vidcapture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+    height = int(vidcapture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
 
-	print fps, width, height
+    print fps, width, height
 
-	# Set up output stream 
-	#fourcc = cv2.cv.CV_FOURCC('X','2','6','4')
-	fourcc = cv2.cv.CV_FOURCC('F','M','P','4')
-	output = cv2.VideoWriter(outfilename, fourcc, fps, (width, height))
-	
-	# Write each frame to file
-	ret, frame = vidcapture.read()
-	while ret:
-		ret, frame = vidcapture.read()
-		output.write(frame)	
-	vidcapture.release()
-	output.release()
+    # Set up output stream
+    #fourcc = cv2.cv.CV_FOURCC('X','2','6','4')
+    fourcc = cv2.cv.CV_FOURCC('F','M','P','4')
+    output = cv2.VideoWriter(outfilename, fourcc, fps, (width, height))
+
+    # Write each frame to file
+    ret, frame = vidcapture.read()
+    while ret:
+        ret, frame = vidcapture.read()
+        output.write(frame)
+    vidcapture.release()
+    output.release()
